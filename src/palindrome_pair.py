8a6be725ed8f415e5841701b112cfda7fe42338c
Mutaawe1 <Mutaawe1@users.noreply.github.com>
2025-04-03 05:29:24 +0000

    Update palindrome_pair to use absolute difference and improve palindrome check

--- src/palindrome_pair.py
+++ src/palindrome_pair.py
@@ -8,7 +8,7 @@ def is_palindrome(num):
     Returns:
         bool: True if the number is a palindrome, False otherwise.
     """
-    return str(num) == str(num)[::-1]
+    return str(abs(num)) == str(abs(num))[::-1]
 
 def palindrome_pair(sorted_nums):
     """
@@ -40,8 +40,9 @@ def palindrome_pair(sorted_nums):
     # Check differences between pairs of numbers
     for i in range(len(sorted_nums)):
         for j in range(i+1, len(sorted_nums)):
-            # Check if the difference is a palindrome
-            if is_palindrome(abs(sorted_nums[j] - sorted_nums[i])):
+            # Check if the absolute difference is a palindrome
+            diff = abs(sorted_nums[j] - sorted_nums[i])
+            if is_palindrome(diff):
                 return True
     
     return False
